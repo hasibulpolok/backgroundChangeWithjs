a95a87673afa69d60525bc50ad729eefad94b423
Hasibul Polok <hasibulpolok.bdn@gmail.com>
2021-08-15 16:19:04 +0600

    bgchangewitheventlistener

---
+++ js/bgcng.js
@@ -0,0 +1,44 @@
+function makeGreen() {
+    document.body.style.backgroundColor = "green";
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = "red";
+}
+
+function makeYellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function makeBlue() {
+    document.body.style.backgroundColor = "blue";
+}
+function makeBlack() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function makePink() {
+    document.body.style.backgroundColor = "pink";
+}
+
+function makeGray() {
+    document.body.style.backgroundColor = "gray";
+}
+
+function makeOrange() {
+    document.body.style.backgroundColor = "orange";
+}
+
+function makeSalmon() {
+    document.body.style.backgroundColor = "salmon";
+}
+
+
+document.getElementById('center').style.textAlign = 'center';
+
+document.getElementsByClassName('button').style.border = '20px solid gray';
+
+let footer = document.getElementById(footer);
+document.createElement('p'),innerText('HasibulPolok');
+footer.append('p');
